0ff38803c3c1af2f5a6df400d7ee6da231e7aa57
VitaliyOvsiienko <106005736+VitaliyOvsiienko@users.noreply.github.com>
2023-03-11 09:38:19 +0000

    Deploying to gh-pages from @ VitaliyOvsiienko/goit-js-hw-08@1f6fe5d81ed040f592b89362dca90fa87b170323 🚀

---
+++ 03-feedback.0a6f097b.js
@@ -0,0 +1,2 @@
+(document.querySelector("feedback-form button"),document.querySelector("feedback-form, textarea")).addEventListener("input",(function(e){const t=e.currentTarget.value;console.log(t)}));
+//# sourceMappingURL=03-feedback.0a6f097b.js.map
