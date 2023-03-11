5943f04d4896434259620e17242b2fb1030e00d7
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-03-11 09:55:40 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,16 @@
 
+const refs = {
+    btn: document.querySelector('feedback-form button'),
+    textarea: document.querySelector('feedback-form, textarea'),
+
+}
+
+// refs.btn.addEventListener('submit]', onFormSubmit);
+refs.textarea.addEventListener('input', OnTextareaInput);
+
+
+function OnTextareaInput(event) {
+    const value = event.currentTarget.value;
+
+    console.log(value);
+}
